37551a88c9ab6cc254bdeb615fce36a524b3d17c
Max Kreuzer <no@no.com>
2026-05-02 11:21:22 +1000

    Created actuator folder and mock functions for testing

---
+++ src/actuators/mock.py
@@ -0,0 +1,10 @@
+# Random actuator functions for testing
+
+
+def waterPlant(ms: float):
+    pass
+    # set GPIO to high for ms time.
+
+def setLEDBrightness(brightness: float):
+    pass
+    # Set PWM speed to brightness, should be in for loop somewhere.
